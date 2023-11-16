8667652878d915fa37c24d60e5db49f772a659e5
Alexei Slautin <slaytin2014@yandex.ru>
2023-11-16 11:41:59 +0300

    Верстка страницы История заказов

--- src/pages/profile-orders-page/profile-orders-page.jsx
+++ src/pages/profile-orders-page/profile-orders-page.jsx
@@ -1,5 +1,13 @@
+import { OrderCard } from "../../components/order-card/order-card";
+import { testOrders } from "../../utils/constant";
+import styles from "./profile-orders-page.module.css";
+
 export const ProfileOrdersPage = () => {
   return (
-      <></>
-  )
-}
+    <div className={`custom-scroll ${styles.wrapper}`}>
+      {testOrders.orders.map((order) => (
+        <OrderCard order={order} key={order._id} />
+      ))}
+    </div>
+  );
+};
